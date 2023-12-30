8774a408e5c5e22af23e770b6e66c0429682c68d
Higunio320 <wojtek123713@gmail.com>
2023-12-30 20:20:06 +0100

    Optimized imports

--- backend/src/main/java/com/notesapp/backend/api/auth/AuthControllerImpl.java
+++ backend/src/main/java/com/notesapp/backend/api/auth/AuthControllerImpl.java
@@ -8,14 +8,9 @@ import com.notesapp.backend.api.auth.interfaces.AuthController;
 import com.notesapp.backend.api.auth.interfaces.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import static com.notesapp.backend.api.auth.constants.AuthControllerConstants.AUTH_API_MAPPING;
--- backend/src/main/java/com/notesapp/backend/api/auth/interfaces/AuthController.java
+++ backend/src/main/java/com/notesapp/backend/api/auth/interfaces/AuthController.java
@@ -5,9 +5,6 @@ import com.notesapp.backend.api.auth.data.AuthenticationRequest;
 import com.notesapp.backend.api.auth.data.AuthenticationResponse;
 import com.notesapp.backend.api.auth.data.RegisterRequest;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 
 public interface AuthController {
 
