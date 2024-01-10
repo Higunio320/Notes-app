e511c0976f82f49933c61ca4fc69a639ed4fb7b8
Higunio320 <wojtek123713@gmail.com>
2024-01-10 21:23:44 +0100

    Added logout and others

--- frontend/src/app/pages/home/notes/notes.component.ts
+++ frontend/src/app/pages/home/notes/notes.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatIconModule} from "@angular/material/icon";
 import {MatButtonModule} from "@angular/material/button";
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
 import {matLogOut} from '@ng-icons/material-icons/baseline';
+import {Router} from "@angular/router";
+import {AuthService} from "../../../core/services/auth/auth.service";
+import {Note} from "../../../core/data/note/note";
+import {NoteService} from "../../../core/services/note/note.service";
+import {MatGridListModule} from "@angular/material/grid-list";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-notes',
@@ -12,12 +18,35 @@ import {matLogOut} from '@ng-icons/material-icons/baseline';
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-    NgIconComponent
+    NgIconComponent,
+    MatGridListModule,
+    NgForOf
   ],
   providers: [provideIcons({matLogOut})],
   templateUrl: './notes.component.html',
   styleUrl: './notes.component.scss'
 })
-export class NotesComponent {
+export class NotesComponent implements OnInit {
+
+  rowspan = 1;
+  colspan = 1;
+  notes : Note[] = [];
+
+  constructor(private router: Router,
+              private authService: AuthService,
+              private noteService: NoteService) {
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['auth/login']);
+  }
+
+  ngOnInit(): void {
+    this.noteService.getAllNotes().subscribe({
+      next: (notes) => this.notes = notes,
+      error: (error) => this.router.navigate(['auth/login'])
+    });
+  }
 
 }
