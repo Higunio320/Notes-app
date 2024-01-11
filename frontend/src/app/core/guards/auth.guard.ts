d2d1bab081940ebffdf3398ba58316c39bfb74fc
Higunio320 <wojtek123713@gmail.com>
2024-01-11 12:01:03 +0100

    Added max width to note edit date

--- frontend/src/app/core/guards/auth.guard.ts
+++ frontend/src/app/core/guards/auth.guard.ts
@@ -6,7 +6,6 @@ export const authGuard: CanActivateFn = (route, state) => {
   if(inject(AuthService).isAuthenticated()) {
     return true;
   } else {
-    inject(Router).navigate(['auth/login']);
     return false;
   }
 };
